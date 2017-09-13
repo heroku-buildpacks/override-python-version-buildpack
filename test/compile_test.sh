30e20daa0efb1ca3e66a8ec79bf4987f1fa642bc
Chris Lambacher <clambacher@heroku.com>
2017-09-13 14:40:37 -0400

    Add tests

---
+++ test/compile_test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+. ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
+
+testCompile()
+{
+
+  capture ${BUILDPACK_HOME}/bin/compile ${BUILD_DIR} ${CACHE_DIR} ${ENV_DIR}
+  assertEquals 0 ${rtrn}
+  assertEquals "" "`cat ${STD_ERR}`"
+  assertEquals "Checking for PYTHON_VERSION_OVERRIDE\nNo PYTHON_VERSION_OVERRIDE found" "`cat ${STD_OUT}`"
+}
+
+testCompileWithPythonVersionOverride()
+{
+  version_expected = "python-3.6.2"
+  echo "$version_expected" > ${ENV_DIR}/PYTHON_VERSION_OVERRIDE
+  capture ${BUILDPACK_HOME}/bin/compile ${BUILD_DIR} ${CACHE_DIR} ${ENV_DIR}
+
+  assertEquals 0 ${rtrn}
+  assertEquals "" "`cat ${STD_ERR}`"
+  assertEquals "Checking for PYTHON_VERSION_OVERRIDE\nOverriding Python version to $version_expected" "`cat ${STD_OUT}`"
+  assertEquals "$version_expected" "`cat ${BUILD_DIR}/runtime.txt`"
+}
